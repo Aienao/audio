a433a179eb24481a99ff75e938b837581165d68b
Aienao <dev@ainoe.com>
2022-07-11 18:53:02 +0800

    adjust

--- src/main/java/com/ainoe/audio/config/Config.java
+++ src/main/java/com/ainoe/audio/config/Config.java
@@ -1,17 +1,14 @@
 package com.ainoe.audio.config;
 
-import org.apache.commons.lang3.StringUtils;
+import com.ainoe.audio.util.ConfigUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Objects;
 import java.util.Properties;
 
@@ -32,13 +29,7 @@ public class Config {
             prop.load(new InputStreamReader(Objects.requireNonNull(Config.class.getClassLoader().getResourceAsStream(CONFIG_FILE)), StandardCharsets.UTF_8));
             AUDIO_HOME = prop.getProperty("audio.home");
             boolean audioHomeAutoCreate = Boolean.parseBoolean(prop.getProperty("audio.home.auto.create", "true"));
-            if (StringUtils.isNotBlank(AUDIO_HOME) && audioHomeAutoCreate) {
-                File home = new File(AUDIO_HOME);
-                if (!home.exists() || !home.isDirectory()) {
-                    Files.createDirectory(Paths.get(AUDIO_HOME));
-                    logger.info("auto create audio home: " + AUDIO_HOME);
-                }
-            }
+            ConfigUtil.createAudioHome(audioHomeAutoCreate);
         } catch (IOException e) {
             logger.error(e.getMessage(), e);
         }
--- src/main/java/com/ainoe/audio/util/ConfigUtil.java
+++ src/main/java/com/ainoe/audio/util/ConfigUtil.java
@@ -4,12 +4,18 @@ import com.ainoe.audio.config.Config;
 import com.ainoe.audio.exception.AudioHomeNotFoundException;
 import com.ainoe.audio.exception.ConfigLostException;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class ConfigUtil {
+
+    static Logger logger = LoggerFactory.getLogger(ConfigUtil.class);
 
     /**
      * 检查audio.home目录是否存在
@@ -21,6 +27,22 @@ public class ConfigUtil {
         Path home = Paths.get(Config.AUDIO_HOME());
         if (!Files.exists(home) || !Files.isDirectory(home)) {
             throw new AudioHomeNotFoundException(Config.AUDIO_HOME());
+        }
+    }
+
+    /**
+     * 自动创建audio.home目录
+     *
+     * @param autoCreate 是否自动创建
+     * @throws IOException
+     */
+    public static void createAudioHome(boolean autoCreate) throws IOException {
+        if (StringUtils.isNotBlank(Config.AUDIO_HOME()) && autoCreate) {
+            File home = new File(Config.AUDIO_HOME());
+            if (!home.exists() || !home.isDirectory()) {
+                Files.createDirectory(Paths.get(Config.AUDIO_HOME()));
+                logger.info("auto create audio home: " + Config.AUDIO_HOME());
+            }
         }
     }
 }
