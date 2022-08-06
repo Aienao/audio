73a7195c05e555603425d59aa008e9f0ab3fbf9f
Aienao <dev@ainoe.com>
2022-08-06 17:12:23 +0800

    adjust

--- src/main/java/com/ainoe/audio/multithread/batch/BatchRunner.java
+++ src/main/java/com/ainoe/audio/multithread/batch/BatchRunner.java
@@ -1,8 +1,3 @@
-/*
- * Copyright(c) 2021 TechSure Co., Ltd. All Rights Reserved.
- * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
- */
-
 package com.ainoe.audio.multithread.batch;
 
 import com.ainoe.audio.multithread.threadpool.CachedThreadPool;
@@ -27,7 +22,6 @@ public class BatchRunner<T> {
         public void setSucceed(boolean succeed) {
             isSucceed = succeed;
         }
-
 
         public Exception getException() {
             return exception;
